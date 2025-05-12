ff8a9cd659f4014698ffb7e29dcc4145a388576c
Krasney5 <137995759+Krasney5@users.noreply.github.com>
2025-05-12 23:03:11 +0300

    .

--- Task/src/main/java/com/example/Task/Repositories/TaskRepository.java
+++ Task/src/main/java/com/example/Task/Repositories/TaskRepository.java
@@ -1,10 +1,14 @@
 package com.example.Task.Repositories;
 
 
+import com.example.Task.Models.Task;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long>{
+public interface TaskRepository extends CrudRepository<Task, Long> {
 
     public List<Task> findByUserName(String name);
 
--- Task/src/main/java/com/example/Task/Services/TaskService.java
+++ Task/src/main/java/com/example/Task/Services/TaskService.java
@@ -1,28 +1,9 @@
 package com.example.Task.Services;
 
+import com.example.Task.Repositories.TaskRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService extends TaskRepository{
-    
-    private TaskRepository repository;
-
-    public List<Task> findByUserName(String name){
-        
-    }
-
-    public Task saveTask(Task task){
-
-
-        repository.save(task)
-        return task;
-    }
-    public Task updateTask(Task task){
-
-    }
-
-    public void delete(Task task){
-
-    }
 
 }
