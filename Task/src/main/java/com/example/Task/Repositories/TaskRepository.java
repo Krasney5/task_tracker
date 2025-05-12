bb64c342a48e25a0fbf007a1e5b4ed958c8863d6
Krasney5 <137995759+Krasney5@users.noreply.github.com>
2025-05-12 23:10:40 +0300

    изменения

--- Task/src/main/java/com/example/Task/Repositories/TaskRepository.java
+++ Task/src/main/java/com/example/Task/Repositories/TaskRepository.java
@@ -12,8 +12,6 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     public List<Task> findByUserName(String name);
 
-    public Task saveTask(Task task);
-
     public Task updateTask(Task task);
 
     public void delete(Task task);
--- Task/src/main/java/com/example/Task/Services/TaskService.java
+++ Task/src/main/java/com/example/Task/Services/TaskService.java
@@ -1,9 +1,24 @@
 package com.example.Task.Services;
 
+import com.example.Task.Models.Task;
 import com.example.Task.Repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class TaskService extends TaskRepository{
+import java.util.List;
 
+@Service
+public class TaskService{
+
+    @Autowired
+    private TaskRepository repository;
+
+    public Task saveTask(Task task){
+        //логика автоматической выставки времени
+        return repository.save(task);
+    }
+
+    public List<Task> findTaskByUserName(String userName){
+        return repository.findByUserName(userName);
+    }
 }
